7a1e1cf07883b584f251a88b1e29fd56bc670e9b
Gonz0134 <m.zawadka115@gmail.com>
2023-07-19 22:07:24 +0200

    plotly

--- game/views.py
+++ game/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render, redirect
 from datetime import datetime, date
 from .models import TodayDate
-import plotly.graph_objects as go
+import plotly.graph_objs as go
 import pandas as pd
 import plotly.offline as opy
+from dateutil.relativedelta import relativedelta
+
 
 def home(request):
     return render(request, 'home.html')
 
 
 def analysis(request):
-    #today date
-    todayObj=TodayDate.objects.all().first()
-    today=todayObj.date
-    dayName=today.strftime("%A")
-    monthName=today.strftime("%B")
+    # today date
+    todayObj = TodayDate.objects.all().first()
+    today = todayObj.date
+    dayName = today.strftime("%A")
+    monthName = today.strftime("%B")
+    daysInGame = 0
+    # chart
+    # 1258 - ostatni dzien z przed rozpoczeciem
+    df = pd.read_csv("Stocks/ndx.txt", sep=',', header=0, encoding='utf-16',nrows=1258+daysInGame)
 
-    #chart
-    df=pd.read_csv("Stocks/ndx.txt", sep=',')
+    fig = go.Figure(
+        data=[go.Candlestick(x=df['Date'], open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'])])
+    config = {
+        'displayModeBar': False
+    }
+    fig.update_layout(hovermode=False,
+                      paper_bgcolor='rgba(0,0,0,0)',
+                      xaxis=dict(fixedrange=True),
+                      yaxis=dict(fixedrange=True),
+                      title="Nasdaq 100 (^NDX)")
+    fig.update_xaxes(range=[today - relativedelta(years=1), today], rangeslider=dict(visible=True))
+    fig.update_yaxes(autorange=True)
+    fig_div = opy.plot(fig, output_type='div', auto_open=False, show_link=False, config=config)
 
-    fig= go.Figure(data=[go.Candlestick(x=df['Date'],open=df['Open'],high=df['High'],low=df['Low'],close=df['Close'])])
-    fig_div = opy.plot(fig, output_type='div', auto_open=False)
-
-    return render(request, 'analysis.html', {'today': today,'day':dayName, 'month':monthName, 'fig':fig_div} )
+    return render(request, 'analysis.html', {'today': today, 'day': dayName, 'month': monthName, 'fig': fig_div})
